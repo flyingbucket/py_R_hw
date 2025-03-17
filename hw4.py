5be4cda77afe291dfeb83b5dcad7e7e9b13ce38d
flyingbucket <2311067@mail.nankai.edu.cn>
2025-03-17 23:13:59 +0800

    hw4

--- hw4.py
+++ hw4.py
@@ -1,22 +1,32 @@
+# -*- coding: utf-8 -*-
 # ---
 # jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.7
 #   kernelspec:
-#       name: python3
-#       display_name: base
-#       language: python3
+#     display_name: base
+#     language: Python
+#     name: python3
 # ---
 
 # %% [markdown]
 # # hw4
 
 # %% [markdown]
-##1.冰淇淋小店：冰淇淋小店是一种特殊的餐馆。编写一个名为IceCreamStand 的
+
+# 1.冰淇淋小店：冰淇淋小店是一种特殊的餐馆。编写一个名为IceCreamStand 的
 # 类，让它继承你为完成第三次作业练习2 或练习5 而编写的Restaurant 类。这两个版本的
 # Restaurant 类都可以，挑选你更喜欢的那个即可。添加一个名为flavors 的属性，用于
 # 存储一个由各种口味的冰淇淋组成的列表。编写一个显示这些冰淇淋的方法。创建一个
 # IceCreamStand 实例，并调用这个方法。
 
 # %% cell 1
+import json
+import os
 
 
 class Restaurant:
@@ -53,6 +63,7 @@ ice_cream_stand = IceCreamStand("DQ", "ice_cream", ["coco", "strawberry"])
 ice_cream_stand.get_flavors()
 
 # %% [markdown]
+
 # 2.管理员：管理员是一种特殊的用户。编写一个名为Admin 的类，让它继承你为
 # 完成第三次作业中练习4 或练习6 而编写的User 类。添加一个名为privileges 的属性，用于存
 # 储一个由字符串（如"can add post"、"can delete post"、"can ban user"等）组成的
@@ -105,7 +116,7 @@ admin.show_privileges()
 # show_privileges()来显示其权限。
 
 
-# %%
+# %% cell 3
 class Privileges:
     def __init__(self, privileges):
         self.privileges = privileges
@@ -117,5 +128,70 @@ privileges = Privileges(
 admin = Admin("John", "Marston", privileges)
 admin.show_privileges()
 
+# 4.编写一个 while 循环，提示用户输入其名字。用户输入其名字后，
+# 在屏幕上打印一句问候语，并将一条访问记录添加到文件 guest_book.txt 中。确保这个
+# 文件中的每条记录都独占一行.
+
+# %% cell 4
+while True:
+    name = input("please enter your name,enter 'q' to quit")
+    with open("gues_book.txt", "a", encoding="utf-8") as file:
+        if name.lower() != "q":
+            file.write(f"{name} visited")
+            print(f"welcome {name}\n")
+        else:
+            print("quiting ...")
+            break
+
 # %% [markdown]
-#
+# 5.编写一个程序，提示用户输入他喜欢的数字，并使用
+# json.dump()将这个数字存储到文件中。再编写一个程序，从文件中读取这个值，并打
+# 印消息“I know your favorite number! It’s _____.”。
+
+# %% cell 5
+f_num = input("entern your favourite number")
+f_num = float(f_num)
+with open("favourite_number.json", "a", encoding="utf-8") as f_obj:
+    json.dump({"favourite_number": f_num}, f_obj)
+    f_obj.write("\n")
+with open("favourite_number.json", "r", encoding="utf-8") as f_obj:
+    data = json.load(f_obj)
+f_num = data["favourite_number"]
+print(f"I know your favorite number! It’s {f_num}")
+
+# %% [markdown]
+# 6.将练习 5中的两个程序合而为一。如果存储了用户喜
+# 欢的数字，就向用户显示它，否则提示用户输入他喜欢的数字并将其存储到文件中。运
+# 行这个程序两次，看看它是否像预期的那样工作。
+
+# %% cell 6
+name = input("please enter your name")
+
+if os.path.exists("favourite_number2.json"):
+    with open("favourite_number2.json", "r", encoding="utf-8") as f_obj:
+        data = json.load(f_obj)
+        print(data)
+    for info in data:
+        if info["name"] == name:
+            print(f"your favourite number is {info['f_num']}")
+        break
+    else:
+        f_num = input(
+            "there is no info of your favourite number. please enter your favourite number "
+        )
+        f_num = float(f_num)
+        new_info = {"name": name, "f_num": f_num}
+        with open("favourite_number2.json", "a", encoding="utf-8") as f_obj:
+            json.dump([new_info], f_obj)
+            f_obj.write("\n")
+else:
+    f_num = input(
+        "there is no info of your favourite number. please enter your favourite number "
+    )
+    f_num = float(f_num)
+    new_info = {"name": name, "f_num": f_num}
+    with open("favourite_number2.json", "a", encoding="utf-8") as f_obj:
+        json.dump([new_info], f_obj)
+        f_obj.write("\n")
+
+        
