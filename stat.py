ef48704a394647b9607b564da1addee8a1add925
fb laptop <2311067@mail.nankai.edu.cn>
2025-03-17 09:26:02 +0800

    hw3

--- stat.py
+++ stat.py
@@ -1,11 +1,12 @@
 import numpy as np
-from scipy.stats import skew,kurtosis
+from scipy.stats import skew, kurtosis
 
-data=[150]*4+[160]*8+[170]*6+[180]*2
-data=np.array(data)
-mean=np.mean(data)
-var=np.var(data,ddof=1)
-skewness=skew(data,bias=True)
-kurt=kurtosis(data,bias=True)
+data = [150] * 4 + [160] * 8 + [170] * 6 + [180] * 2
+data = np.array(data)
+mean = np.mean(data)
+var = np.var(data, ddof=1)
+skewness = skew(data, bias=True)
+kurt = kurtosis(data, bias=True)
 
-print(mean,"\n",var,"\n",skewness,"\n",kurt,"\n")
+print(mean, "\n", var, "\n", skewness, "\n", kurt, "\n")
+print("all done!")
